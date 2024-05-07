b5acee30aae54184c37bd81e1e3ef60ecc1ec6b9
arzymat01 <arzymatarkabaev01@gmail.com>
2024-05-08 00:01:18 +0600

    firebaseR

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,9 +1,13 @@
-import 'dart:async';
+import 'models/models.dart';
 
 abstract class UserRepository {
   Stream<MyUser?> get user;
+
   Future<MyUser> signUp(MyUser myUser, String password);
+
   Future<void> setUserData(MyUser user);
-  Future<void> signIn(String email, String passwor);
+
+  Future<void> signIn(String email, String password);
+
   Future<void> logOut();
 }
