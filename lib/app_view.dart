751994c32056d26fe3117866af74e7fa932d08aa
Arzymat01 <arzymatarkabaev01@gmail.com>
2024-05-15 23:55:06 +0600

    Ui

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc_bloc.dart';
+import 'package:pizza_app/screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({Key? key}) : super(key: key);
@@ -7,6 +10,22 @@ class MyAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pizza Delivery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.light(
+              background: Colors.grey.shade100,
+              onBackground: Colors.black,
+              primary: Colors.blue,
+              onPrimary: Colors.white)),
+      home: BlocBuilder<AuthenticationBlocBloc, AuthenticationState>(
+        builder: ((context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return HomeSreen();
+          } else {
+            return WelcomeScreen();
+          }
+        }),
+      ),
     );
   }
 }
---
+++ lib/screens/home/views/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeSreen extends StatelessWidget {
+  const HomeSreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
