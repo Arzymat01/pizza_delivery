bab3f8249f838c85d9543e3f51d1614bc37ac514
Arzymat01 <arzymatarkabaev01@gmail.com>
2024-05-19 01:17:21 +0600

    home_screen  Ui первый шаг началсы

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc_bloc.dart';
+import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:pizza_app/screens/auth/views/welcome_screen.dart';
 import 'package:pizza_app/screens/home/views/home_screen.dart';
 
@@ -22,7 +23,11 @@ class MyAppView extends StatelessWidget {
       home: BlocBuilder<AuthenticationBlocBloc, AuthenticationState>(
         builder: ((context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return HomeSreen();
+            return BlocProvider(
+              create: (context) => SignInBloc(
+                  context.read<AuthenticationBlocBloc>().userRepository),
+              child: const HomeSreen(),
+            );
           } else {
             return WelcomeScreen();
           }
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 
 class HomeSreen extends StatelessWidget {
   const HomeSreen({Key? key}) : super(key: key);
@@ -15,6 +18,7 @@ class HomeSreen extends StatelessWidget {
               'assets/8.png',
               scale: 14,
             ),
+            const SizedBox(width: 8),
             const Text(
               'PIZZA',
               style: TextStyle(
@@ -23,6 +27,44 @@ class HomeSreen extends StatelessWidget {
               ),
             )
           ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(SignOutRequired());
+            },
+            icon: const Icon(CupertinoIcons.cart),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.arrow_right_to_line),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            childAspectRatio: 9 / 16,
+          ),
+          itemCount: 4,
+          itemBuilder: (context, int i) {
+            return Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade400,
+                      blurRadius: 5,
+                      offset: const Offset(3, 5),
+                    )
+                  ]),
+            );
+          },
         ),
       ),
     );
