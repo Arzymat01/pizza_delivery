b73127b4c62b615ac5786d3845b7d0a4f82bb447
Arzymat01 <arzymatarkabaev01@gmail.com>
2024-05-24 21:46:48 +0600

    errror firebse

---
+++ android/app/src/main/kotlin/com/exapay/pizza_delivery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exapay.pizza_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/pizza_repository/lib/src/models/pizza.dart
@@ -0,0 +1,23 @@
+// class Pizza {
+//   String pizzaId;
+//   String picture;
+//   bool isVeg;
+//   int spicy;
+//   String name;
+//   String decription;
+//   double price;
+//   double discount;
+//   List<Macros> macros;
+
+//   Pizza(
+//     this.pizzaId,
+//     this.picture,
+//     this.isVeg,
+//     this.spicy,
+//     this.name,
+//     this.decription,
+//     this.price,
+//     this.macros,
+//     this.discount,
+//   );
+// }
